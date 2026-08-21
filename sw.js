87210ba2c58373e268f31b5f065509923bb2a12c
YasinC37 <cuceogluyasinn@gmail.com>
2026-08-21 18:01:54 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const CACHE='896-fit-v8';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
